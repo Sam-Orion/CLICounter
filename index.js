96f278ec31f120d0847ffc7354d82ac0ee4c49dc
Sam-Orion <121442461+Sam-Orion@users.noreply.github.com>
2025-03-18 00:43:15 +0530

    Added sentence counting functionality

--- index.js
+++ index.js
@@ -21,4 +21,19 @@ program.command('words')
         });
     });
 
+
+program.command('sentences')
+    .description('Count the number of words in a file')
+    .argument('<file>', 'file to count')
+    .action((file) => {
+        fs.readFile(file, 'utf8', (err, data) => {
+            if (err) {
+              console.log(err);
+            } else {
+              const lines = data.split('\n').length;
+              console.log(`There are ${lines} lines in ${file}`);
+            }
+        });
+    });
+
 program.parse();
